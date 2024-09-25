70c5b113d9577b0eae24733d7bb00b6cb00db2b5
Greenca14 <124871992+Greenca14@users.noreply.github.com>
2024-09-25 14:30:51 +0500

    Add files via upload

---
+++ lab1/lab1/Main1.1.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+
+using namespace std;
+
+typedef enum States { Normal, Slash, Comment, Star, SlashSlash };
+
+int main(int argc, char** argv) {
+	ifstream inFile("in.c");
+	ofstream outFile("out.c");
+
+	if (!inFile.is_open() || !outFile.is_open()) {
+		cout << "Error opening file." << endl;
+		return 1;
+		inFile.close();
+		outFile.close();
+	}
+	else {
+		States State = Normal;
+		char c;
+
+		while ((c = inFile.get()) != EOF) { 
+			switch (State)
+			{
+			case Normal:
+				if (c == '/') {
+					State = Slash;
+				}
+				else {
+					outFile.put(c);
+				}
+				break;
+			case Slash:
+				if (c == '*') {
+					State = Comment;
+				}
+				else if (c == '/') {
+					State = SlashSlash;
+				}
+				else{
+					State = Normal;
+					outFile.put('/');
+					outFile.put(c);
+				}
+				break;
+			case SlashSlash:
+				if (c == '\n') {
+					outFile.put('\n');
+					State = Normal;
+				}
+				break;
+			case Comment:
+				if (c == '*') {
+					State = Star;
+				}
+				break;
+			case Star:
+				if (c == '/') {
+					State = Normal;
+				}
+				else if (c == '*') {
+				}
+				else {
+					State = Comment;
+				}
+				break;
+			}
+		}
+	}
+
+	inFile.close();
+	outFile.close();
+	return 0;
+}
